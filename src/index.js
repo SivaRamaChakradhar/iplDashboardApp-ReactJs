d53fc563cfd3805e71f895ae7c6d50edda925026
sivaramachakradhar <sivaramisetti700@gmail.com>
2025-12-05 17:24:48 +0000

    deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/iplDashboardApp-ReactJs">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
